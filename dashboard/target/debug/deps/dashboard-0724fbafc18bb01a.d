5ace066ba2f54fdbbe62d57c61338e7936b316c4
James Arrowsmith <ja774@exeter.ac.uk>
2025-08-20 22:09:36 +0100

    remove .env lol

--- dashboard/target/debug/deps/dashboard-0724fbafc18bb01a.d
+++
@@ -1,5 +0,0 @@
-C:\dev\mctui\dashboard\target\debug\deps\dashboard-0724fbafc18bb01a.d: src\main.rs
-
-C:\dev\mctui\dashboard\target\debug\deps\libdashboard-0724fbafc18bb01a.rmeta: src\main.rs
-
-src\main.rs:
--- dashboard/target/debug/deps/dashboard-7c416934e2489f3b.d
+++
@@ -1,5 +0,0 @@
-C:\dev\mctui\dashboard\target\debug\deps\dashboard-7c416934e2489f3b.d: src\main.rs
-
-C:\dev\mctui\dashboard\target\debug\deps\libdashboard-7c416934e2489f3b.rmeta: src\main.rs
-
-src\main.rs:

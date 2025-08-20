9e517bf509df0c993ac2c690874e2fd173457a4d
James Arrowsmith <ja774@exeter.ac.uk>
2025-08-20 22:14:27 +0100

    reconfigure gitignore cache

--- collector/migrations/20250820140427_create_events_table.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-CREATE TABLE events(
-    id UUID,
-    event_type VARCHAR(40) NOT NULL,
-    player_name VARCHAR(40),
-    created_at TIMESTAMP,
-    payload JSONB
-);
--- collector/migrations/20250820155250_set_created_at_default.sql
+++
@@ -1,3 +0,0 @@
--- Add migration script here
-ALTER TABLE events
-ALTER COLUMN created_at SET DEFAULT NOW();

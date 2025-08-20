c1970ab3e1b4cf1c37c34913d62e332316c7677c
James Arrowsmith <ja774@exeter.ac.uk>
2025-08-20 20:59:15 +0100

    mc-plugin hello world

---
+++ mc-plugin/mc-plugin/src/main/java/com/jarrowsmith123/Main.java
@@ -0,0 +1,14 @@
+package com.jarrowsmith123;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
